f497752799394bf4739c6343dc89d4000018d831
argon-dev <argon@argon.dev>
2025-10-05 14:18:26 -0500

    Use cargo publish --workspace

--- rust/publish.sh
+++ rust/publish.sh
@@ -1,7 +1,3 @@
 #!/bin/bash -ex
 
-cargo publish -p esexpr-derive
-cargo publish -p esexpr
-cargo publish -p esexpr-json
-cargo publish -p esexpr-binary
-cargo publish -p esexpr-text
+cargo publish --workspace --exclude esexpr-utils --exclude esexpr-examples
